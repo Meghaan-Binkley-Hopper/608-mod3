a0a97f4efe4616eced8fb29eaf9f42a222d5d9c2
Meghaan-Binkley-Hopper <97639670+Meghaan-Binkley-Hopper@users.noreply.github.com>
2022-02-04 19:37:55 -0600

    Add files via upload

---
+++ built-in-min-max.py
@@ -0,0 +1,9 @@
+#Finding maximum and minimum using the Python built-in fumctions
+max1 = max(12, 27, 36) #Finding Maximum
+print(max1)
+max2 = max(12.3, 45.6, 9.7) #Finding Maximum
+print(max2)
+max3 = max('yellow', 'red', 'orange') #Finding Maximum
+print(max3)
+min1 = min(15, 9, 27, 14)
+print(min1)
---
+++ calculator.py
@@ -0,0 +1,7 @@
+"""Using the math module to make calculations"""
+
+import math #calling the math module
+
+print(math.sqrt(900))
+
+print(math.floor(9.2))
